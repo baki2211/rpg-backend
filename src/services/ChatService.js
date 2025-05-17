3a65b373ca3c4e46eadd2b992aede77a2fa3bf84
AngeloDiscoveryED <apane@discoveryed.com>
2025-05-17 23:00:09 +0100

    update character name in chat

--- src/services/ChatService.js
+++ src/services/ChatService.js
@@ -1,9 +1,11 @@
 import { AppDataSource } from '../data-source.js';
 import { ChatMessage } from '../models/chatMessageModel.js';
 import { MoreThan } from 'typeorm';
+import { Character } from '../models/characterModel.js';
 
 export class ChatService {
   chatRepository = AppDataSource.getRepository(ChatMessage);
+  characterRepository = AppDataSource.getRepository(Character);
 
   async getMessagesByLocation(locationId) {
     // Fetch messages from the past 5 hours
@@ -15,7 +17,21 @@ export class ChatService {
   }
 
   async addMessage(locationId, userId, username, message) {
-    const chatMessage = this.chatRepository.create({ location: { id: locationId }, userId, username, message });
+    const character = await this.characterRepository.findOne({ where: { userId, isActive: true } });
+    if (!character) {
+      throw new Error('No active character found for this user.');
+    }
+
+    const chatMessage = this.chatRepository.create({
+      location: { id: locationId },
+      userId,
+      characterId: character.id,
+      message,
+      senderName: character.name,
+      username: character.name,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
     return this.chatRepository.save(chatMessage);
   }
 }
