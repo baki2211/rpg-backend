006f3aa7a58bb721ea3bf86a41fdcae0e2e541d9
baki2211 <angipane@hotmail.it>
2025-06-15 12:20:45 +0100

    remove migration

--- src/migrations/1710000000000-RemoveScalingFactor.js
+++
@@ -1,9 +0,0 @@
-export class RemoveScalingFactor1710000000000 {
-    async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "skills" DROP COLUMN "scalingFactor"`);
-    }
-
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "skills" ADD "scalingFactor" float`);
-    }
-} 
